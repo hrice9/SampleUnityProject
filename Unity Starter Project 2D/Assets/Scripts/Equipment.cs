43671c75431915f95ab8f441050e072706c061cb
Hamilton Rice <44306440+hrice9@users.noreply.github.com>
2020-01-28 10:46:44 -0500

    Update file system

--- Unity Starter Project 2D/Assets/Scripts/Equipment.cs
+++ Unity Starter Project 2D/Assets/Scripts/Equipment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 [System.Serializable]
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
 public class Equipment : Item
 {
     // add equipment here
--- Unity Starter Project 2D/Assets/Scripts/Item.cs
+++ Unity Starter Project 2D/Assets/Scripts/Item.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class Item : ScriptableObject
 {
+    new public string name = "New Item";
+
     // put stats, icons, etc here
 
     public virtual void Use()
